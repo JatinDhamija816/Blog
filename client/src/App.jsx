1fb74d77313baa9dd1d58f4fb6d30124f08ec89f
JatinDhamija816 <jdhamija816@gmail.com>
2024-04-10 13:22:51 +0530

    LoginPageSetup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
+import Login from "./components/account/Login"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Login />
     </>
   )
 }
