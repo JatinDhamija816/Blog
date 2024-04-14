295fcd72db0cf8b4fcda1f77ff387d6e99aafc64
JatinDhamija816 <jdhamija816@gmail.com>
2024-04-14 21:42:48 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -7,16 +7,13 @@ import userRoute from './routes/userRoutes.js'
 import blogRoute from './routes/blogRoutes.js'
 
 dotenv.config()
-
 const app = express()
-
 app.use(cors())
 app.use(bodyParser.json())
-
 app.use('/api/v1/user', userRoute)
 app.use('/api/v1/blog', blogRoute)
 
-const Port = process.env.PORT || 8000
+const Port = process.env.PORT || 5000
 app.listen(Port, (req, res) => {
     console.log("Server Start")
 })
